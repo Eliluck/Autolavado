168d5f03ac54cfda65302c254a96e196ab8cfb36
Eliel Camejo <elielfernandomartinez@gmail.com>
2024-05-05 22:51:30 -0500

    autolavado 1.3.0
    modificacion en los routers

--- app-autolavado/routes/web.php
+++ app-autolavado/routes/web.php
@@ -19,47 +19,47 @@ Route::get('/', function () {
 });
 
 // Ruta para verificar la existencia de un RFC, esta ruta es del controlador CheckRFCController
-Route::post('/check-rfc', [CheckRFCController::class, 'checkRFC'])->name('check.rfc');
+Route::post('/check-rfc', [CheckRFCController::class, 'checkRFC']);
 
 // Ruta para obtener la información del cliente por RFC, esta ruta es del controlador ClientController
-Route::get('/client/{rfc}', [ClientController::class, 'getByRFC'])->name('client.getByRFC');
+Route::get('/client/{rfc}', [ClientController::class, 'getByRFC']);
 
 // Ruta para obtener las colonias basadas en un código postal, esta ruta es del controlador DirSatColController
-Route::get('/colonies/{cp}', [DirSatColController::class, 'getByCP'])->name('colonies.getByCP');
+Route::get('/colonies/{cp}', [DirSatColController::class, 'getByCP']);
 
 // Ruta para obtener los códigos postales basados en un código de país, esta ruta es del controlador DirSatCPController
-Route::get('/postal-codes/{countryCode}', [DirSatCPController::class, 'byCountryCode'])->name('postalCodes.byCountryCode');
+Route::get('/postal-codes/{countryCode}', [DirSatCPController::class, 'byCountryCode']);
 
 // Ruta para obtener el estado basado en un código postal, esta ruta es del controlador DirSatEdoController
-Route::get('/state-by-cp/{cp}', [DirSatEdoController::class, 'getEstadoPorCP'])->name('state.getByCP');
+Route::get('/state-by-cp/{cp}', [DirSatEdoController::class, 'getEstadoPorCP']);
 
 // Ruta para obtener la localidad basada en un código postal, esta ruta es del controlador DirSatLocController
-Route::get('/locality-by-cp/{postalCode}', [DirSatLocController::class, 'getByPostalCode'])->name('locality.getByPostalCode');
+Route::get('/locality-by-cp/{postalCode}', [DirSatLocController::class, 'getByPostalCode']);
 
 // Ruta para obtener los municipios basados en un código de municipio, esta ruta es del controlador DirSatMunController
-Route::get('/municipalities-by-code/{municipioCode}', [DirSatMunController::class, 'getByMunicipioCode'])->name('municipalities.getByMunicipioCode');
+Route::get('/municipalities-by-code/{municipioCode}', [DirSatMunController::class, 'getByMunicipioCode']);
 
 // Ruta para descargar archivos PDF y XML basados en un identificador de venta, esta ruta es del controlador DownloadController
-Route::post('/download-pdf-xml', [DownloadController::class, 'downloadPDFXML'])->name('download.pdfxml');
+Route::post('/download-pdf-xml', [DownloadController::class, 'downloadPDFXML']);
 
 // Ruta para facturar un ticket, esta ruta es del controlador FacturacionController
-Route::post('/facturar-ticket', [FacturacionController::class, 'facturarTicket'])->name('facturacion.facturarTicket');
+Route::post('/facturar-ticket', [FacturacionController::class, 'facturarTicket']);
 
 /*seccion del controlador ReportVentaController*/
 
 // Ruta para obtener todas las ventas, con la opción de filtrar por rango de fechas
-Route::get('/ventas', [ReportVentaController::class, 'index'])->name('ventas.index');
+Route::get('/ventas', [ReportVentaController::class, 'index']);
 
 // Ruta para mostrar una venta específica junto con los datos del cliente
-Route::get('/ventas/{id}', [ReportVentaController::class, 'show'])->name('ventas.show');
+Route::get('/ventas/{id}', [ReportVentaController::class, 'show']);
 
 // Ruta para crear una nueva venta
-Route::post('/ventas', [ReportVentaController::class, 'store'])->name('ventas.store');
+Route::post('/ventas', [ReportVentaController::class, 'store']);
 
 /* fin de la seccion del controlador ReportVentaController*/
 
 // Ruta para actualizar los datos fiscales de un cliente, la ruta del controlador UpdateDatosFiscalesController
-Route::put('/update-datos-fiscales', [UpdateDatosFiscalesController::class, 'updateDatosFiscales'])->name('datosFiscales.update');
+Route::put('/update-datos-fiscales', [UpdateDatosFiscalesController::class, 'updateDatosFiscales']);
 
 // Ruta para obtener documentos de ventas filtrados por RFC y fechas, esta ruta es del controlador VentaController
-Route::post('/ventas/documentos', [VentaController::class, 'getDocumentos'])->name('ventas.getDocumentos');
+Route::post('/ventas/documentos', [VentaController::class, 'getDocumentos']);
